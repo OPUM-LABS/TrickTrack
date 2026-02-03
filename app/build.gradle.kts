01fb9932a4fab402cc0d70599a6e814c4e4c7017
OPUM-Labs <app@opum.ch>
2026-02-03 08:16:28 +0100

    Changed version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.3.10"
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
