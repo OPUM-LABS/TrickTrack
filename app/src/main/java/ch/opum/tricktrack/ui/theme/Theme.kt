fe78166cae741c898e890154b233951df43e66b2
OPUM-Labs <app@opum.ch>
2026-08-30 09:47:59 +0200

    fixed wrong status bar icon color when switching theme

--- app/src/main/java/ch/opum/tricktrack/ui/theme/Theme.kt
+++ app/src/main/java/ch/opum/tricktrack/ui/theme/Theme.kt
@@ -8,7 +8,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import android.app.Activity
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -56,6 +60,15 @@ fun TrickTrackTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     MaterialTheme(
